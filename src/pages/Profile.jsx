7e2bfce356edcc869d15c7c3fb18997e39499fb9
Ahmed Asif <aa0322811@gmail.com>
2025-05-05 13:20:05 +0500

    first commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -47,8 +47,8 @@ const Profile = () => {
         <>
             <section className=" w-full h-[calc(100vh-80px)] bg-pastel">
                 <form onSubmit={handleformsubmit}>
-                    <div className="w-full h-full px-20">
-                        <div className="w-full min-h-[50vh] border border-deep-teal bg-white shadow-2xl px-[100px] py-[10px] rounded-3xl mt-[30px]">
+                    <div className="w-full h-full px-20 py-20">
+                        <div className="w-full min-h-[50vh] border border-deep-teal bg-white shadow-2xl px-[100px] py-[10px] rounded-3xl ">
                             <div className=" flex items-center gap-5">
                                 <div className="relative">
                                     <Avatar className="p-10 bg-seaform rounded-full">
@@ -80,7 +80,7 @@ const Profile = () => {
                                     />
                                 </div>
                             </div>
-                            <div className="w-full grid grid-cols-1 lg:grid-cols-4 mt-5 gap-5">
+                            <div className="w-full grid grid-cols-1 lg:grid-cols-4 mt-5 gap-5 text-deep-teal">
                                 <div className="w-full relative">
                                     <Label className="font-semibold">Email</Label>
                                     <Input
